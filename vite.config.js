0aa05e04d3968dd4d4116591b6297bd55b7b7c82
maslo0396 <ilya97750@gmail.cm>
2026-04-06 14:20:05 +0300

    swiper repair

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,15 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base: '/SKYFORCE/', // Важно! Имя репозитория
   // другие настройки
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]'
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['swiper']
+  }
 })
